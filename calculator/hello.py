5f470335ddff27e1d27c5ede6aeb76683f322a2d
Jevon <iheartbenzene@gmail.com>
2019-10-29 23:45:03 -0400

    getting used to forms

--- calculator/hello.py
+++ calculator/hello.py
@@ -6,10 +6,12 @@ import sys
 
 # from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 # from PyQt5.QtWidgets import QApplication, QHBoxLayout, QPushButton, QWidget
-from PyQt5.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
+# from PyQt5.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QFormLayout, QLineEdit, QWidget
 
-# application = QApplication(sys.argv)
-# window = QWidget()
+application = QApplication(sys.argv)
+window = QWidget()
+
 # # window.setWindowTitle('PyQt5 App')
 # window.setWindowTitle('QHBoxLayout')
 # ''' There also exists QVBoxLayout to give a vertical look ''' 
@@ -23,24 +25,28 @@ from PyQt5.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
 # # hello_message = QLabel('<h1>Hello World!</h1>', parent = window)
 # # hello_message.move(60, 15)
 # window.setLayout(hlayout)
-# window.show()
-# sys.exit(application.exec_())
 
-application = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('QGridLayout')
-layout = QGridLayout()
-layout.addWidget(QPushButton('Button 0, 0'), 0, 0)
-layout.addWidget(QPushButton('Button 0, 1'), 0, 1)
-layout.addWidget(QPushButton('Button 0, 2'), 0, 2)
-layout.addWidget(QPushButton('Button 1, 0'), 1, 0)
-layout.addWidget(QPushButton('Button 1, 1'), 1, 1)
-layout.addWidget(QPushButton('Button 1, 2'), 1, 2)
-layout.addWidget(QPushButton('Button 2, 0'), 2, 0)
-layout.addWidget(QPushButton('Button 2, 1'), 2, 1)
-layout.addWidget(QPushButton('Button 2, 2'), 2, 2)
-layout.addWidget(QPushButton('Button 3, 1 + 3 columns span'), 3, 0, 3, 3)
+# window.setWindowTitle('QGridLayout')
+# layout = QGridLayout()
+# layout.addWidget(QPushButton('Button 0, 0'), 0, 0)
+# layout.addWidget(QPushButton('Button 0, 1'), 0, 1)
+# layout.addWidget(QPushButton('Button 0, 2'), 0, 2)
+# layout.addWidget(QPushButton('Button 1, 0'), 1, 0)
+# layout.addWidget(QPushButton('Button 1, 1'), 1, 1)
+# layout.addWidget(QPushButton('Button 1, 2'), 1, 2)
+# layout.addWidget(QPushButton('Button 2, 0'), 2, 0)
+# layout.addWidget(QPushButton('Button 2, 1'), 2, 1)
+# layout.addWidget(QPushButton('Button 2, 2'), 2, 2)
+# layout.addWidget(QPushButton('Button 3, 1 + 3 columns span'), 3, 0, 3, 3)
+# window.setLayout(layout)
 
+window.setWindowTitle('QFormLayout')
+layout = QFormLayout()
+layout.addRow('Name: ', QLineEdit())
+layout.addRow('Age: ', QLineEdit())
+layout.addRow('Class: ', QLineEdit())
+layout.addRow('Likes: ', QLineEdit())
+layout.addRow('Dislikes: ', QLineEdit())
 window.setLayout(layout)
 
 window.show()
